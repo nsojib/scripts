ccabde1409da6c9eb02418286caf213c7bc82bc4
nsojib <noushad.sust@gmail.com>
2025-04-26 20:20:53 -0400

    diffp

---
+++ diffp.sh
@@ -0,0 +1,11 @@
+
+cd ~/diffusion_policy
+conda env create -f conda_environment.yaml --solver=libmamba
+
+# cd ~/diffusion_policy
+# mkdir data
+# wget https://diffusion-policy.cs.columbia.edu/data/training/robomimic_image.zip
+
+
+
+
